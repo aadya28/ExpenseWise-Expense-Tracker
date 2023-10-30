481c6db207d60a20e41c2f4c7ecf1a59e0bb9a0f
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-10-31 01:53:17 +0530

    Added a footer and changed styling of the login and signUp page.

--- script.js
+++ script.js
@@ -135,4 +135,19 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').slice(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
